45ac611022510ee3831f396564dae8d6b7dd74eb
Antonio <ntoniohu@gmail.com>
2020-08-04 20:49:19 +0000

    Add documentation link in welcome tab

--- R/welcome.R
+++ R/welcome.R
@@ -2,7 +2,11 @@
 #' @importFrom shiny fluidRow p
 welcomePageUI <- function() {
 
-  shiny::fluidRow(
-    shiny::p("Welcome to FSLRetho, a GUI to the Rethomics.")
+  documentation_url <- "https://docs.google.com/document/d/1cClaDaiQHbexiEVFpVo7bN-09ZgYSw-WLSYToWYuZU4"
+
+  shiny::div(
+    shiny::p("Welcome to FSLRetho, an R Shiny web application designed to make Rethomics easy."),
+    shiny::p("A comprehensive documentation"),
+    shiny::a(href=documentation_url, target="_blank", "is available")
   )
 }
