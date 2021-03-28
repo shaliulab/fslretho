7be89659e6d3d52b2513758311989938fb308aca
Antonio Ortega <ntoniohu@gmail.com>
2021-03-28 23:46:09 +0200

    Add script to generate external data

---
+++ inst/dev.R
@@ -0,0 +1,27 @@
+library(data.table)
+library(RSQLite)
+library(fslretho)
+
+
+# fslretho::make_toy_ethoscope_sqlite(nrows=1e4)
+
+metadata <- data.table::data.table(region_id=1, machine_name="ETHOSCOPE_000", date="2021-03-27", reference_hour=7)
+data.table::fwrite(x = metadata, file = "inst/extdata/ethoscope_metadata/metadata.csv")
+
+metadata <- fslscopr::link_ethoscope_metadata(metadata, result_dir = system.file("extdata/ethoscope_data/results", package = "fslretho"))
+raw_data <- fslscopr::load_ethoscope(metadata = metadata, reference_hour = NA)
+fslretho::saveRDS_behavr(object = raw_data, file = "inst/extdata/raw_data.rds")
+stopifnot(!is.null(data.table::key(raw_data)))
+
+scored_data <- fslsleepr::sleep_annotation(
+  data = raw_data,
+  velocity_correction_coef=0.0048,
+  min_time_immobile=300,
+  time_window_length=10
+)
+
+fslretho::saveRDS_behavr(object = scored_data, file = "inst/extdata/scored_data.rds")
+
+con <- RSQLite::dbConnect(RSQLite::SQLite(), list.files(system.file("extdata/ethoscope_data/results", package = "fslretho"), full.names = TRUE, recursive = T),  RSQLite::SQLITE_RO)
+data.table::as.data.table(RSQLite::dbGetQuery(con, "SELECT * FROM VAR_MAP"))
+RSQLite::dbDisconnect(con)
