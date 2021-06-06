2cb5e82400746b0ac1d9ce042d68d9be113eadd1
Antonio Ortega <ntoniohu@gmail.com>
2021-06-06 20:38:03 +0200

    Score module test works

--- tests/testthat/test-score.R
+++ tests/testthat/test-score.R
@@ -1,48 +1,52 @@
-library(testthat)
-library(data.table)
-library(shiny)
-library(waldo)
-
-devtools::load_all()
 context("data load")
 
-test_that("app loads files as expected", {
+test_that("app scores when supplying any of the 3 score parameters", {
 
-  loaded_data <- fslretho::readRDS_behavr(file = file.path(system.file("extdata", package="fslretho"), "raw_data.rds"))
-  data.table::key(loaded_data)
-
+  dt_raw <- behavr::toy_ethoscope_data()
+  data.table::key(dt_raw)
   raw_data <- reactiveValues(
-    ethoscope = reactiveValues(
-      data = loaded_data,
-      name = "metadata.csv",
-      time = 1
-    )
+    ethoscope=reactiveValues(data=dt_raw, name="toy", time=0),
+    dam=reactiveValues(data=NULL,name=NULL,time=NULL)
   )
 
+  testServer(scoreDataServer, args = list("raw_data" = raw_data), {
 
-  trigger <- reactiveVal(0)
-  testServer(scoreDataServer, args = list("raw_data" = raw_data, "trigger" = trigger), {
+    expect_null(monitors_dt$ethoscope$data)
 
     session$setInputs(
       velocity_correction_coef=0.0048,
       min_time_immobile=300,
       time_window_length=10
     )
-    # session$flushReact()
+
+
+    expect_is(monitors_dt$ethoscope$data, "behavr")
+    expect_true("asleep" %in% colnames(monitors_dt$ethoscope$data))
+    #expect_equal(cnd$message, "Velocity correction coefficient: 0.0048\n")
     scored_data <- session$getReturned()
+    expect_identical(scored_data$ethoscope$data, monitors_dt$ethoscope$data)
 
-    target <- as.data.table(
-      readRDS_behavr(file = file.path(system.file("extdata", package="fslretho"), "scored_data.rds"))
+    mean_asleep <- mean(monitors_dt$ethoscope$data$asleep)
+
+    session$setInputs(
+      velocity_correction_coef=0.0003
     )
-    ref <- as.data.table(
-      scored_data$ethoscope$data
+
+    mean_asleep2 <- mean(monitors_dt$ethoscope$data$asleep)
+    expect_true(mean_asleep2 < mean_asleep)
+    # the coefficient decreased,
+    # so we are less lenient with how much the fly can move and still
+    # be considered sleep
+
+    n_rows <- nrow(monitors_dt$ethoscope$data)
+
+    session$setInputs(
+      time_window_length=20
     )
+    n_rows2 <- nrow(monitors_dt$ethoscope$data)
 
+    expect_true(nrows2 < nrows) # the bins are bigger so there are less bins
 
-    min(target$t)
-    min(ref$t)
-    x <- waldo::compare(target, ref, ignore_attr=TRUE)
-    expect_length(x, 0)
 
   })
 })
