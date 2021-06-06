ac45c2c4822c2a42e577ce834693da028e8a3371
Antonio Ortega <ntoniohu@gmail.com>
2021-06-06 20:38:36 +0200

    Clean testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,24 +1,2 @@
 library(testthat)
-library(fslretho)
-
-library(fslbehavr)
-library(fslscopr)
-library(logging)
-library(R6)
-library(fslbehavr)
-library(fslscopr)
-library(fslretho)
-library(ggplot2)
-library(fslscopr)
-library(fslggetho)
-library(R6)
-library(logging)
-library(magrittr)
-data(dt)
-
-setLevel(level = "WARNING")
-
-
-setLevel(level = "WARNING")
-test_check("fslretho")
-    
+library(behavr)
