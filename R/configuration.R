dcb5efc72406a728e501ccf6782b083aceecfe88
Antonio Ortega <ntoniohu@gmail.com>
2021-06-13 19:16:45 +0200

    Update configuration

--- R/configuration.R
+++ R/configuration.R
@@ -7,12 +7,12 @@
 #' via a config file
 #'
 #' @name FSLREthoConfiguration
+#' @export
 #' @importFrom R6 R6Class
 #' @importFrom rjson fromJSON toJSON
 #' @method initialize something
 #' @field content List of configuration items
 #' @field config_file Default configuration file path
-#'
 FSLRethoConfiguration <- R6::R6Class(classname = "FSLREthoConfiguration", public = list(
 
   content = list(),
@@ -21,61 +21,28 @@ FSLRethoConfiguration <- R6::R6Class(classname = "FSLREthoConfiguration", public
   #' Initialize a configuration instance
   #' Called by running `FSLRethoConfiguration$new()`
   #' @param config_file Path to the default configuration file
-  initialize = function(config_file = file.path(c(file.path(Sys.getenv("HOME"), ".config"), "/etc"), "fslretho.conf")) {
+  initialize = function(config_file = "/etc/fslretho.conf") {
 
-    content <- scopr::scoprConfiguration$new()$content
-    content <- append(content, list("debug" = FALSE, "stop_backups" = TRUE, port = 3838, testing=FALSE))
+    content <- list()
+
+    # link scopr conf
+    content$scopr <- scopr::scoprConfiguration$new()$content
+    # TODO link damr conf
+    # content$damr <- damr::damrConfiguration$new(testing=testing)$content
+    content$damr <- list(folders = list(results = list(path = "/DAM_data/results")))
+    content <- append(content, list("debug" = FALSE, port = 3838, testing=FALSE))
 
     content$folders <- append(content$folders, list(
-      "dam" = list(
-        "path" = "/DAM_data/results",
-        "description" = "A path to a folder containing MonitorXX.txt files"
-        ),
       "ethoscope_sessions" = list(
         "path" = "/ethoscope_data/sessions",
-        "description" = "A path to a folder containign rds files for fast reloading of data loaded in a previous run.
+        "description" = "A path to a folder containing rds files for fast reloading of data loaded in a previous run.
         The files are created when the user presses the save button in fslretho. The whole dataset loaded there is saved to a single rds file."
       )
     ))
 
-
-
-    index <- which(sapply(config_file, is.writable) & file.exists(config_file))
-    if(length(index) == 0) index <- 1
-    self$config_file <- get_writable_path(config_file[index])
-    stopifnot(is.writable(dirname(self$config_file)))
+    self$config_file <- config_file
     self$content <- content
-    self$load()
-    self$verify()
-  },
-
-  #' Make sure all folders that fslretho needs are writable
-  #' if they are not, recreate them as needed in the HOME folder
-  #' @param config_file Configuration file path
-  verify = function(config_file = NULL) {
-
-    content <- self$content
-    old_content <- content
-    folders <- content$folders
-
-      # check if folder is writable
-      for (i in 1:length(folders)) {
-        f <- folders[[i]]
-        if (!is.writable(path = dirname(f$path))) {
-          writable_f <- file.path(Sys.getenv("HOME"), ".fslretho", f$path)
-          content$folders[[i]]$path <- writable_f
-        } else {
-          writable_f <- f$path
-        }
-
-        # create folder if does not exist
-        if (!dir.exists(writable_f)) dir.create(writable_f, recursive = TRUE)
-      }
-    self$content <- content
-    self$save()
-
-    if (!identical(self$content, old_content))
-        self$save(self$config_file)
+    self$load(self$config_file)
   },
 
   #' Save the configuration in to a config_file
@@ -83,11 +50,14 @@ FSLRethoConfiguration <- R6::R6Class(classname = "FSLREthoConfiguration", public
   #' Save the configuration stored in self$content
   #' If the passed config_file is null, use the instance's default
   #' @param config_file Configuration file path
-  save = function(config_file = NULL) {
-    json <- rjson::toJSON(self$content)
+  save = function(config_file) {
 
-    if (is.null(config_file))
-      config_file <- self$config_file
+    content <- self$content
+    # unlink modules
+    content$damr <- NULL
+    content$scopr <- NULL
+
+    json <- rjson::toJSON(content)
 
     if(self$content$debug) message(paste0("Saving ", config_file))
     write(x = json, file = config_file)
@@ -98,12 +68,10 @@ FSLRethoConfiguration <- R6::R6Class(classname = "FSLREthoConfiguration", public
   #' If the passed file does not exist create it with the conf in the default file
   #' If it exists, load its contents and update the configuration
   #' @param config_file Configuration file path
-  load = function(config_file=NULL) {
+  load = function(config_file) {
 
-    if (is.null(config_file))
-      config_file <- self$config_file
-
-    if (!file.exists(config_file) & !is.null(config_file))
+    # if the config file is not does not exist or if it is empty
+    if (!file.exists(config_file) | file.size(config_file) == 0)
       self$save(config_file)
     else {
       if(self$content$debug) message(paste0("Loading ", config_file))
