99a961848971986ef912f7b31b1052dd408da4a9
Antonio Ortega <ntoniohu@gmail.com>
2021-06-13 19:22:12 +0200

    Remove fsl dependenceis and create a testing metadata from scopr

--- inst/dev.R
+++ inst/dev.R
@@ -8,8 +8,8 @@ library(fslretho)
 metadata <- data.table::data.table(region_id=1, machine_name="ETHOSCOPE_000", date="2021-03-27", reference_hour=7)
 data.table::fwrite(x = metadata, file = "inst/extdata/ethoscope_metadata/metadata.csv")
 
-metadata <- fslscopr::link_ethoscope_metadata(metadata, result_dir = system.file("extdata/ethoscope_data/results", package = "fslretho"))
-raw_data <- fslscopr::load_ethoscope(metadata = metadata, reference_hour = NA)
+metadata <- scopr::link_ethoscope_metadata(metadata, result_dir = system.file("extdata/ethoscope_data/results", package = "fslretho"))
+raw_data <- scopr::load_ethoscope(metadata = metadata, reference_hour = NA)
 fslretho::saveRDS_behavr(object = raw_data, file = "inst/extdata/raw_data.rds")
 stopifnot(!is.null(data.table::key(raw_data)))
 
@@ -25,3 +25,9 @@ fslretho::saveRDS_behavr(object = scored_data, file = "inst/extdata/scored_data.
 con <- RSQLite::dbConnect(RSQLite::SQLite(), list.files(system.file("extdata/ethoscope_data/results", package = "fslretho"), full.names = TRUE, recursive = T),  RSQLite::SQLITE_RO)
 data.table::as.data.table(RSQLite::dbGetQuery(con, "SELECT * FROM VAR_MAP"))
 RSQLite::dbDisconnect(con)
+
+
+metadata <- scopr:::metadata
+metadata$reference_hour <- 7
+data.table::fwrite(metadata, file = "inst/extdata/ethoscope_metadata.csv")
+
