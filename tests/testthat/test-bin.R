966557f6d7047b576b531b211d20adb345fbd435
antortjim <ntoniohu@gmail.com>
2021-06-17 11:12:30 +0200

    Finish testing bin module

--- tests/testthat/test-bin.R
+++ tests/testthat/test-bin.R
@@ -1,39 +1,87 @@
-test_that("app chooses ethoscope input source as expected", {
+test_that("binServer works", {
 
   dt <- behavr::toy_ethoscope_data()
   dt_annot <- sleepr::sleep_annotation(data = dt)
-  stopifnot(data.table::key(dt_annot) == "id")
 
-  unified_data <- reactiveValues(
-      data = dt_annot,
-      name = "metadata.csv",
-      time = 1
-  )
+  input_rv <- reactiveValues(data = NULL, name = NULL, time = NULL)
 
-
-  testServer(binDataServer, args = list("data" = unified_data, "main"=TRUE), {
+  testServer(binDataServer, args = list(input_rv = input_rv), {
 
     session$setInputs(
+      summary_time_window = 30, # unit is min
       summary_FUN = "sleep amount",
-      summary_time_window = 30,
       y = "asleep"
     )
 
+    input_rv$data <- dt_annot
+    input_rv$name <- "toy"
+    # not able to trigger
+    session$flushReact()
+    expect_null(output_rv$data)
+    # setting the time (together with flushReact) should manage to trigger the module
+    input_rv$time = Sys.time()
+    session$flushReact()
+    expect_is(output_rv$data, "data.table") # the module is rejoining the behavr
+    mean_sleep <- round(mean(output_rv$data$asleep), 2)
+    length_sleep <- as.integer(nrow(output_rv$data))
+    sd_sleep <- round(sd(output_rv$data$asleep), 2)
 
-    target <- as.data.table(
-      behavr::bin_all(
-        loaded_data,
-        x = "t",
-        y = "asleep",
-        x_bin_length = behavr::mins(30),
-        FUN = mean
+    expect_true(
+      all(
+        c(mean_sleep, sd_sleep, length_sleep) == c(0.40, 0.31, 240)
       )
     )
+  })
+})
 
-    ref <- as.data.table(
-      binned_data$data
+test_that("binServer can be passed a preprocessing function", {
+
+
+  dt <- behavr::toy_ethoscope_data()
+  dt_annot <- sleepr::sleep_annotation(data = dt)
+
+  input_rv <- reactiveValues(data = NULL, name = NULL, time = NULL)
+
+  testServer(binDataServer, args = list(input_rv = input_rv, preproc_FUN = bout_analysis, var="asleep"), {
+
+    session$setInputs(
+      summary_time_window = 30, # unit is min
+      summary_FUN = "sleep amount",
+      y = "duration"
     )
 
-    expect_length(x, 0)
+    input_rv$data <- dt_annot
+    input_rv$name <- "toy"
+    input_rv$time = Sys.time()
+    session$flushReact()
+
+    expect_true("duration" %in% colnames(output_rv$data))
+    expect_equal(nrow(output_rv$data), 177) # 177 bouts of sleep
+    expect_equal(round(mean(output_rv$data$duration / 60), 2), 15.33)
+  })
+})
+
+
+test_that("binServer can be passed a preprocessing function and a differnt var", {
+
+  dt <- behavr::toy_ethoscope_data()
+  dt_annot <- sleepr::sleep_annotation(data = dt)
+
+  input_rv <- reactiveValues(data = NULL, name = NULL, time = NULL)
+
+  testServer(binDataServer, args = list(input_rv = input_rv, preproc_FUN = bout_analysis, var="moving"), {
+
+    session$setInputs(
+      summary_time_window = 30, # unit is min
+      summary_FUN = "sleep amount",
+      y = "duration"
+    )
+
+    input_rv$data <- dt_annot
+    input_rv$name <- "toy"
+    input_rv$time = Sys.time()
+    session$flushReact()
+
+    expect_true(nrow(output_rv$data) == 240)
   })
 })
