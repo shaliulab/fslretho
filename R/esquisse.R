f824602ec93b57a826848871048b7efc5084055f
Antonio Ortega <ntoniohu@gmail.com>
2021-06-21 20:10:40 +0200

    Apply some sensible column type coercion before passing data to esquisse. Specially useful to coerce region_id to factor so each region id is treated as a label and not a number, which makes for a selector and not a slider in the UI

--- R/esquisse.R
+++ R/esquisse.R
@@ -1,13 +1,30 @@
+coerce_columns <- function(data) {
+
+  data$region_id <- as.factor(data$region_id)
+  data$datetime <- as.factor(data$region_id)
+  data$fly_count <- as.factor(data$fly_count)
+  data
+}
+
 #' @importFrom esquisse esquisse_server
-#'
 esquisseModuleServer <- function(id, input_rv, ...) {
   moduleServer(
     id,
     function(input, output, session) {
 
+      esquisse_rv <- reactiveValues(
+        data = NULL, name = NULL, time = NULL
+      )
+
+      observeEvent(c(input_rv$time, input_rv$data), {
+        esquisse_rv$data <- coerce_columns(input_rv$data)
+        esquisse_rv$name <- input_rv$name
+        esquisse_rv$time <- input_rv$time
+      })
+
       output_rv <- esquisse::esquisse_server(
         "esquisse",
-        data_rv = input_rv,
+        data_rv = esquisse_rv,
         data_modal = FALSE,
         ...
       )
