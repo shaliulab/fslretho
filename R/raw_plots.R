85ca5633f57d54edb8c44150b6a9e974a6974bb0
antortjim <ntoniohu@gmail.com>
2021-07-12 11:39:01 +0200

    Fix raw plots module and implement time selector

--- R/raw_plots.R
+++ R/raw_plots.R
@@ -41,9 +41,20 @@ rawPlotsServer <- function(id, sleep_data) {
         # browser()
         metadata$file_info <- lapply(metadata$file_info, function(x) list(path=x, filename=basename(x)))
         d <- load_ethoscope(metadata, cache = CACHE, reference_hour = NA)
+        d <- d[t > input$time_range[1] * 3600 & t < input$time_range[2] * 3600,]
+        # we still want raw data resolution, i.e. annotation
+        # for every raw data point
+        # therefore the time_window_length must be very small
+        sd_annot <- as.data.table(sd_inprogress_annotation(d, time_window_length = 0.1))
+        d <- behavr::rejoin(d)
+
+        setkey(d, id, t)
+        setkey(sd_annot, id, t)
+        d <- d[sd_annot, on=c("t"), roll=T]
+        d <- as.data.table(d)
+        d <- d[rowSums(is.na(d)) == 0, ]
+
         d
-
-
         row_indices <- seq(1, nrow(d), by=input$downsample)
         d <- d[row_indices,]
 
@@ -54,8 +65,15 @@ rawPlotsServer <- function(id, sleep_data) {
         }
 
         if (input$sd_only) {
+          # browser()
+          before_filter <- nrow(d)
           d <- d[isTRUE(sd_on),]
+          if (before_filter > 0 & nrow(d) == 0) {
+            validate(need(FALSE, "No data under SD"))
+          }
         }
+
+        # browser()
 
         output_rv$raw$data <- d
         output_rv$raw$name <- sleep_data$name
@@ -67,11 +85,12 @@ rawPlotsServer <- function(id, sleep_data) {
       sleep_dataset <- eventReactive(input$button, {
         req(sleep_data$data)
         req(animal_id())
-        d <- behavr::rejoin(sleep_data$data)[id == animal_id()]
+        d <- behavr::rejoin(sleep_data$data)[id == animal_id() & t > input$time_range[1] * 3600 & t < input$time_range[2] * 3600]
         d
       })
 
       output$plot_raw <- renderPlot({
+        validate(need(nrow(raw_dataset()) > 0, "No data available"))
         ggplot(data = raw_dataset(), aes(x = t, y = x)) + ggplot2::geom_point() +
           ggetho::scale_x_hours() + ggetho::geom_ld_annotations(color=NA, height=1, alpha=0.2) +
           facet_wrap("id")
@@ -100,8 +119,9 @@ rawPlotsUI <- function(id) {
     inputPanel(
       actionButton(ns("button"), "Plot"),
       checkboxInput(ns("sd_only"), "SD only", value = TRUE),
-      numericInput(ns("downsample"), label = "Downsample", value = 10, min = 1, max = 100, step = 1),
-      uiOutput(ns("animal_id_ui"))
+      numericInput(ns("downsample"), label = "Downsample", value = 100, min = 1, max = 1000, step = 1),
+      uiOutput(ns("animal_id_ui")),
+      sliderInput(ns("time_range"), label = "Time range", min = 0, max = behavr::days(7) / 3600, step = 6, value = c(0, behavr::days(7) / 3600), )
     ),
     plotOutput(ns("plot_raw")),
     plotOutput(ns("plot_sleep"))
