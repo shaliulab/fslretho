0e05ed6b60440d336932ec94f7871b4e9235dfab
Antonio Ortega <ntoniohu@gmail.com>
2021-07-14 13:51:57 +0200

    Set the x limit explicitly so the other plots inherit the same limits

--- R/raw_plots.R
+++ R/raw_plots.R
@@ -112,8 +112,12 @@ rawPlotsServer <- function(id, sleep_data, interactions_data) {
       plot_x <- reactive({
 
         validate(need(nrow(raw_dataset()) > 0, "No data available"))
+        x_limits <- c(
+          floor(min(raw_dataset()$t) / 3600) * 3600,
+          ceiling(min(raw_dataset()$t) / 3600) * 3600,
+        )
         ggplot(data = raw_dataset(), aes(x = t, y = x)) + ggplot2::geom_point() +
-          ggetho::scale_x_hours() + ggetho::geom_ld_annotations(color=NA, height=1, alpha=0.2) +
+          ggetho::scale_x_hours(limits = x_limits) + ggetho::geom_ld_annotations(color=NA, height=1, alpha=0.2) +
           facet_wrap("id")
       })
 
